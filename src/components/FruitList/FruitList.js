9c0dd88fb3a0268233656fe0475fdb30679efc95
Eric Matlock <ermatlock@gmail.com>
2022-08-04 16:46:59 -0400

    Add fruitlist query

--- src/components/FruitList/FruitList.js
+++ src/components/FruitList/FruitList.js
@@ -1,10 +1,29 @@
 import './FruitList.css'
+import { useQuery } from "@tanstack/react-query";
+
+const fetchFruits = async () => {
+  const res = await fetch("https://fe-cors-proxy.herokuapp.com", {
+    headers: {
+      "Target-URL": "https://www.fruityvice.com/api/fruit/all",
+    },
+  });
+  return res.json();
+};
 
 const FruitList = () => {
+  const { data, status } = useQuery(["fruits"], fetchFruits);
   return (
-    <div>
-      <h1>This is my Fruit List</h1>
-    </div>
+    <main>
+      {status === "error" && <p>Error fetching data</p>}
+        {status === "loading" && <p>Fetching data...</p>}
+        {status === "success" && (
+          <div>
+          {data.map((fruit) => (
+            <p key={fruit.id}>{fruit.name}</p>
+          ))}
+        </div>
+        )}
+    </main>
   )
 }
 
