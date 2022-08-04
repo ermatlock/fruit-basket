5aa1b1309cf7e1f40dcf7b1264a1d7f9a8cb7a7f
Eric Matlock <ermatlock@gmail.com>
2022-08-03 23:27:04 -0400

    Update app with routes to FruitList and FruitTracker

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+// import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom"
+import { fetchData } from "../../apiCalls";
+import "./App.css";
+import Loader from "../Loader/Loader";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import FruitList from "../FruitList/FruitList";
+import FruitTracker from "../FruitTracker/FruitTracker";
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<FruitList />} />
+        <Route path="/fruit-tracker" element={<FruitTracker />} />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
