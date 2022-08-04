a2b35f7f422a8715e28be2177375b331d638aea3
Eric Matlock <ermatlock@gmail.com>
2022-08-04 16:48:30 -0400

    Remove unused imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,9 @@
-// import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom"
-import { fetchData } from "../../apiCalls";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Loader from "../Loader/Loader";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import FruitList from "../FruitList/FruitList";
 import FruitTracker from "../FruitTracker/FruitTracker";
-
 
 const App = () => {
   return (
