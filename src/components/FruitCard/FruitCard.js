21418fc2adcc23a0dfbe691c00f0b33c16431714
Eric Matlock <ermatlock@gmail.com>
2022-08-04 22:05:58 -0400

    Add card elements and styling

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -1,7 +1,15 @@
-import './FruitCard.css'
+import "./FruitCard.css";
 
-const FruitCard = () => {
-  <div></div>
-}
+const FruitCard = ({ id, name, nutritions }) => {
+  return(
+  <div className="fruit-card" id={id}>
+    <h3>{name}</h3>
+    <h4>Nutrition Stats:</h4>
+    <p>Sugar: {nutritions.sugar}</p>
+    <p>Protein: {nutritions.protein}</p>
+    <p>Fat: {nutritions.fat}</p>
+  </div>
+  )
+};
 
 export default FruitCard;
