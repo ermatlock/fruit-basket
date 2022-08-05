01bd95397c75fd5b3da5f5dcab9e6a81dacce00f
Eric Matlock <ermatlock@gmail.com>
2022-08-05 00:15:13 -0400

    Rearrange layout

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -3,15 +3,15 @@ import "./FruitCard.css";
 const FruitCard = ({ id, name, nutritions }) => {
   return (
     <div className="fruit-card" id={id}>
-      <h3>{name}</h3>
-      <label for="add">Add to basket</label>
-      <input type="checkbox" id={id} name="add" />
-      <label for="amount">Amount (1-50):</label>
-      <input type="number" id="amount" name="amount" min="1" max="50" />
+      <h2>{name}</h2>
       <h4>Nutrition Stats:</h4>
       <p>Sugar: {nutritions.sugar}</p>
       <p>Protein: {nutritions.protein}</p>
       <p>Fat: {nutritions.fat}</p>
+      <input type="checkbox" id='add' name="add" />
+      <label for="add">Add to basket</label>
+      <input type="number" id="amount" name="amount" min="1" max="50" />
+      <label for="amount">Amount (1-50):</label>
     </div>
   );
 };
