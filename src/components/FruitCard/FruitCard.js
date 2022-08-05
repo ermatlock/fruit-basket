6472b4fb4a764174d8b72d1d1d40ca52a41a471b
Eric Matlock <ermatlock@gmail.com>
2022-08-04 22:37:45 -0400

    Update inputs and styling

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -1,15 +1,19 @@
 import "./FruitCard.css";
 
 const FruitCard = ({ id, name, nutritions }) => {
-  return(
-  <div className="fruit-card" id={id}>
-    <h3>{name}</h3>
-    <h4>Nutrition Stats:</h4>
-    <p>Sugar: {nutritions.sugar}</p>
-    <p>Protein: {nutritions.protein}</p>
-    <p>Fat: {nutritions.fat}</p>
-  </div>
-  )
+  return (
+    <div className="fruit-card" id={id}>
+      <h3>{name}</h3>
+      <label for="add">Add to basket</label>
+      <input type="checkbox" id={id} name="add" />
+      <label for="amount">Amount (1-50):</label>
+      <input type="number" id="amount" name="amount" min="1" max="50" />
+      <h4>Nutrition Stats:</h4>
+      <p>Sugar: {nutritions.sugar}</p>
+      <p>Protein: {nutritions.protein}</p>
+      <p>Fat: {nutritions.fat}</p>
+    </div>
+  );
 };
 
 export default FruitCard;
