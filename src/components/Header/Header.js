c7a20e833f4f32d344013382aa337b0fb07894f8
Eric Matlock <ermatlock@gmail.com>
2022-08-05 20:53:53 -0400

    Add search bar to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import "./Header.css";
+import FruitSearch from "../FruitSearch/FruitSearch";
 
 const Header = () => {
-  return(
-  <header>
-    <img src="./images/fruit-basket.png" />
-    <h1>FruitBasket</h1>
-  </header>
-  )
+  return (
+    <header>
+      <div className="header-left">
+        <img className="logo" alt="wooden fruit cart brimming over with fruit" src="./images/fruit-basket.png" />
+        <h1>FruitBasket</h1>
+      </div>
+      <div className="header-middle">
+        <FruitSearch/>
+      </div>
+      <div className="header-right">
+        <button className="nav-btn">Go To Basket</button>
+        <img className="basket" alt="empty wicker basket" src="./images/wicker-basket.png" />
+
+      </div>
+    </header>
+  );
 };
 
 export default Header;
