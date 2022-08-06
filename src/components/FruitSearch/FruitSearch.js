a445ba7780b9f55fee6c11ed4cc2638d982e7c3a
Eric Matlock <ermatlock@gmail.com>
2022-08-05 20:55:20 -0400

    Add input and styling

--- src/components/FruitSearch/FruitSearch.js
+++ src/components/FruitSearch/FruitSearch.js
@@ -1,7 +1,25 @@
 import './FruitSearch.css'
+import { useState } from 'react'
 
-const FruitSearch = () => {
-  <div></div>
+const FruitSearch = ({ searchFruit }) => {
+  const [searchValue, setSearchValue] = useState("")
+
+  const handleSearchInputChanges = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    searchFruit(e.target.value);
+  };
+
+  return (
+    <form className="search">
+      <input
+        value={searchValue}
+        placeholder="Enter a fruit"
+        onChange={handleSearchInputChanges}
+        type="text"
+      />
+    </form>
+
+  )
 }
 
 export default FruitSearch;
