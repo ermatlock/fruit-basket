43adb84cefa588519cfb87c7def9a3ff9d04377e
Eric Matlock <ermatlock@gmail.com>
2022-08-06 16:32:08 -0400

    Update search bar

--- src/components/FruitSearch/FruitSearch.js
+++ src/components/FruitSearch/FruitSearch.js
@@ -4,17 +4,19 @@ import { useState } from 'react'
 const FruitSearch = ({ searchFruit }) => {
   const [searchValue, setSearchValue] = useState("")
 
-  const handleSearchInputChanges = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleInput = (e) => {
     setSearchValue(e.target.value);
     searchFruit(e.target.value);
   };
 
   return (
     <form className="search">
+      <label htmlFor="search" >Search for fruit</label>
       <input
+        id="search"
         value={searchValue}
-        placeholder="Enter a fruit"
-        onChange={handleSearchInputChanges}
+        // placeholder="Enter a fruit"
+        onChange={handleInput}
         type="text"
       />
     </form>
