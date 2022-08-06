7d2bd662c1512195ab06d519398a1e57c971514a
Eric Matlock <ermatlock@gmail.com>
2022-08-06 16:30:12 -0400

    Fix for to htmlFor

--- src/components/FruitCard/FruitCard.js
+++ src/components/FruitCard/FruitCard.js
@@ -7,13 +7,13 @@ const FruitCard = ({ id, name, nutritions }) => {
         <h2>{name}</h2>
         <ul>
           Nutrition Stats:
-          <li>Sugar: {nutritions.sugar}</li>
-          <li>Protein: {nutritions.protein}</li>
-          <li>Fat: {nutritions.fat}</li>
+          <li>Sugar: {nutritions.sugar}g</li>
+          <li>Protein: {nutritions.protein}g</li>
+          <li>Fat: {nutritions.fat}g</li>
         </ul>
       </div>
       <div className="fruit-card-right">
-        <label for="amount">Choose amount (1-50):</label>
+        <label htmlFor="amount">Choose amount (1-50):</label>
         <input type="number" id="amount" name="amount" min="1" max="50" />
         <button className="button-fruit-card">add to basket</button>
       </div>
