5f836a586d806db19c545f00fab429e4ebec22c8
Eric Matlock <ermatlock@gmail.com>
2022-08-07 17:32:43 -0400

    Set tracked fruits to empty array
    Also edit staringFruits data to reflect simplified fruit objects

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -12,28 +12,19 @@ const fetchFruits = async () => {
 
 const startingFruits = [{
   "name": "Apple",
-  "id": 6,
-  "family": "Rosaceae",
-  "order": "Rosales",
+  "id": 100,
   "nutritions": {
-      "carbohydrates": 11.4,
       "protein": 0.3,
       "fat": 0.4,
-      "calories": 52,
       "sugar": 10.3
   },
   "stock": 5
 }, {
-  "genus": "Prunus",
   "name": "Apricot",
-  "id": 35,
-  "family": "Rosaceae",
-  "order": "Rosales",
+  "id": 101,
   "nutritions": {
-      "carbohydrates": 3.9,
       "protein": 0.5,
       "fat": 0.1,
-      "calories": 15,
       "sugar": 3.2
   },
   "stock": 2
@@ -46,7 +37,7 @@ const DataContextProvider = ({ children }) => {
   const [searchValue, setSearchValue] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(true);
-  const [trackedFruits, setTrackedFruits] = useState(startingFruits)
+  const [trackedFruits, setTrackedFruits] = useState([])
   const [totalStock, setTotalStock ] = useState(0);
 
   useEffect(() => {
@@ -117,7 +108,8 @@ const DataContextProvider = ({ children }) => {
         errorMessage,
         isLoading,
         totalStock,
-        trackedFruits
+        trackedFruits,
+        setTrackedFruits,
       }}
     >
       {data && children}
