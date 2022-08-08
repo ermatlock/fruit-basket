5e7a68f747d423a01449f0708c6d6acc8552d8bc
Eric Matlock <ermatlock@gmail.com>
2022-08-08 14:38:21 -0400

    Add social links and images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,22 @@
-import './Footer.css'
+import "./Footer.css";
+import github from "../../assets/images/github.svg";
+import linkedin from "../../assets/images/linkedin.svg";
 
 const Footer = () => {
   return (
     <footer>
-    
+      <h2 className="footer-text">
+        FruitBasket | Developed by Eric Matlock |
+        <a href="https://github.com/ermatlock" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="github social icon" />
+        </a>
+        |
+        <a href="https://www.linkedin.com/in/ericmatlock/" target="blank" rel="noopener noreferrer">
+          <img src={linkedin} alt="linkedin social icon" />
+        </a>
+      </h2>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
