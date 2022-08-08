60abd62c4ee27e0201d1a5e21c10ef78878fa0ab
Eric Matlock <ermatlock@gmail.com>
2022-08-07 23:51:49 -0400

    Add email address sign in if none is in DataContext

--- src/components/FruitList/FruitList.js
+++ src/components/FruitList/FruitList.js
@@ -1,19 +1,45 @@
 import "./FruitList.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DataContext } from "../../contexts/DataContext";
 import Loader from "../Loader/Loader";
 import Error from "../Error/Error";
 import FruitCard from "../FruitCard/FruitCard";
+import Swal from "sweetalert2";
 
 const FruitList = () => {
-  const { errorMessage, filteredFruits, isLoading } = useContext(DataContext);
+  const {
+    errorMessage,
+    filteredFruits,
+    isLoading,
+    emailAddress,
+    setEmailAddress,
+  } = useContext(DataContext);
+
+  useEffect(() => {
+    if (!emailAddress) {
+      fireLogin()
+    }
+  },[])
+
+  const fireLogin = () => {
+    Swal.fire({
+      title: 'Please enter your email address',
+      input: 'email',
+      inputLabel: 'Your email address',
+      inputPlaceholder: 'Enter your email address'
+    }).then((result) => {
+      setEmailAddress(result.value)
+    })
+  }
 
   return (
     <main>
       <div className="content-container fade-in">
-      {!errorMessage && !isLoading && <div className="main-heading">
-        <h2>Select a fruit and add it to the basket</h2>
-      </div>}
+        {!errorMessage && !isLoading && (
+          <div className="main-heading">
+            <h2>Select a fruit and add it to the basket</h2>
+          </div>
+        )}
         {isLoading && <Loader />}
         {errorMessage && <Error errorMessage={errorMessage} />}
         {filteredFruits && (
