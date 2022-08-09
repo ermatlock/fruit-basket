80e5890cc8bdd9827fd4b9d59e94c1657e4856ec
Eric Matlock <ermatlock@gmail.com>
2022-08-08 21:36:01 -0400

    Format prettier

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,35 +12,40 @@ const Header = () => {
 
   return (
     <header>
-      <div className="header-left">
-        <div className="logo-container">
-          <img
-            className="logo"
-            alt="wooden fruit cart brimming over with fruit"
-            src={logo}
-          />
-          <h1>FruitBasket</h1>
+      <div className="header-container">
+        <div className="header-left">
+          <div className="logo-container">
+            <img
+              className="logo"
+              alt="wooden fruit cart brimming over with fruit"
+              src={logo}
+            />
+            <h1>FruitBasket</h1>
+          </div>
         </div>
-      </div>
-      <div className="header-middle">
-        {location.pathname === "/" && <FruitSearch />}
-      </div>
-      <div className="header-right">
-        {location.pathname !== "/" ? (
-          <Link to="/">
-            <button className="nav-btn">view fruit list</button>
-          </Link>
-        ) : (
-          <Link to="/fruit-tracker">
-            <button className="nav-btn">track my fruits</button>
-          </Link>
-        )}
-
-        <div className="basket-container">
-          <span className="notification-icon--fixed">
-            <small className="notification-badge">{totalStock}</small>
-          </span>
-          <img alt="empty wicker basket" src={stockBasket} />
+        <div className="header-middle">
+          {location.pathname === "/" && <FruitSearch />}
+        </div>
+        <div className="header-right">
+          {location.pathname !== "/" ? (
+            <Link to="/">
+              <button className="nav-btn">view fruit list</button>
+            </Link>
+          ) : (
+            <Link to="/fruit-tracker">
+              <button className="nav-btn">track my fruits</button>
+            </Link>
+          )}
+          <div className="basket-container">
+            <span className="notification-icon--fixed">
+              <small className="notification-badge">{totalStock}</small>
+            </span>
+            <img
+              className="basket"
+              alt="empty wicker basket"
+              src={stockBasket}
+            />
+          </div>
         </div>
       </div>
     </header>
