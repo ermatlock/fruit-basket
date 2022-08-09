8680624177bfc44d81a102e51308061edcee88b0
Eric Matlock <ermatlock@gmail.com>
2022-08-08 21:33:37 -0400

    Rearrange layout for responsive design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,21 @@ import linkedin from "../../assets/images/linkedin.svg";
 const Footer = () => {
   return (
     <footer>
-      <h2 className="footer-text">
-        FruitBasket | Developed by Eric Matlock |
-        <a href="https://github.com/ermatlock" target="_blank" rel="noopener noreferrer">
-          <img src={github} alt="github social icon" />
-        </a>
-        |
-        <a href="https://www.linkedin.com/in/ericmatlock/" target="blank" rel="noopener noreferrer">
-          <img src={linkedin} alt="linkedin social icon" />
-        </a>
-      </h2>
+      <h2 className="footer-text">FruitBasket | Developed by Eric Matlock</h2>
+      <a
+        href="https://github.com/ermatlock"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="github social icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ericmatlock/"
+        target="blank"
+        rel="noopener noreferrer"
+      >
+        <img src={linkedin} alt="linkedin social icon" />
+      </a>
     </footer>
   );
 };
